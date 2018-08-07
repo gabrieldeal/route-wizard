ce3a6418e46fa2622eb5915044bd05134a433d47
Gabriel M Deal <gabriel@dicksonlabs.com>
2018-08-07 00:14:19 +0000

    Make page title & what-is-this optional in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,62 +8,67 @@ import { StaticQuery, graphql } from 'gatsby';
 import Header from './header';
 import './layout.css';
 
-const Layout = ({ children, data, pageTitle, whatIsThis }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children, data, pageTitle, whatIsThis }) => {
+  const title = pageTitle && (
+    <Tooltip title={whatIsThis}>
+      <div style={{ paddingBottom: 10 }}>
+        <Typography variant="title" style={{ display: 'inline' }}>
+          {pageTitle}
+        </Typography>
+        <a style={{ diplay: 'inline', paddingLeft: 10 }} href="#">
+          (What is this?)
+        </a>
+      </div>
+    </Tooltip>
+  );
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={(data) => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: 'description',
-              content: 'Build route spreadsheets from GeoJSON files',
-            },
-            {
-              name: 'keywords',
-              content: 'geojson, track, mountain, hike, long-distance',
-            },
-          ]}
-        />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '1rem 1.0875rem 1.45rem',
-          }}
-        >
-          <Tooltip title={whatIsThis}>
-            <div style={{ paddingBottom: 10 }}>
-              <Typography variant="title" style={{ display: 'inline' }}>
-                {pageTitle}
-              </Typography>
-              <a style={{ diplay: 'inline', paddingLeft: 10 }} href="#">
-                (What is this?)
-              </a>
-            </div>
-          </Tooltip>
-
-          {children}
-        </div>
-      </>
-    )}
-  />
-);
+      `}
+      render={(data) => (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              {
+                name: 'description',
+                content: 'Build route spreadsheets from GeoJSON files',
+              },
+              {
+                name: 'keywords',
+                content: 'geojson, track, mountain, hike, long-distance',
+              },
+            ]}
+          />
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '1rem 1.0875rem 1.45rem',
+            }}
+          >
+            {title}
+            {children}
+          </div>
+        </>
+      )}
+    />
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-  pageTitle: PropTypes.string.isRequired,
-  whatIsThis: PropTypes.element.isRequired,
+  pageTitle: PropTypes.string,
+  whatIsThis: PropTypes.element,
 };
 
 export default Layout;
